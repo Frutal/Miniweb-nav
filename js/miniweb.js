1f5572aaa918826d283d3214f7fb49755550494e
Frutal <sergifrutos@gmail.com>
2015-06-29 01:28:47 +0200

    ja es droppable i apareixen les fixes

--- js/miniweb.js
+++ js/miniweb.js
@@ -3,25 +3,31 @@ $( document ).ready(function() {
 
  $(".draggable").draggable({ revert: true });
 
- $("#droppable").droppable({
+ $(".droppable").droppable({
   activeClass: 'ui-state-hover',
   hoverClass: 'ui-state-active',
+
   // uncomment the line below to only accept the .correct class..
   // Note: the "drop" function will not be called if not ".correct"
   //  accept : '.correct',
   drop: function(event, ui) {
-   // alternative method:
-   // if (ui.draggable.find('p').text() == "1") {
-   if (ui.draggable.is('.correct')) {
-    $(this).addClass('ui-state-highlight').find('p').html('You got it!');
+    $(this).addClass('noesveu');
+    var nom=ui.draggable.attr('name');
+    $('div#fitxes_personal').children().each(
+      function()
+      {
+        if($(this).attr("name")===nom)
+        {
+          $(this).removeClass("noesveu");
+          $(this).addClass("esveu");
+}});
+    //$('#ffrutal').show();
+
     // cloning and appending prevents the revert animation from still occurring
-    ui.draggable.clone(true).css('position', 'inherit').appendTo($(this));
-    ui.draggable.remove();
-   } else {
-    $('#droppable > p').html('Not that one!')
-    setTimeout(function(){ $('#droppable > p').html('Drop here'); }, 1000);
-   }
+    //ui.draggable.clone(true).css('position', 'inherit').appendTo($(this));
+  //  ui.draggable.remove();
+
+
   }
  });
-
 });
